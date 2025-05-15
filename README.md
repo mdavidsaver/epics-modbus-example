45bf67ead0b39b8fd61a2ace2aabd1fa89babdda
Michael Davidsaver <mdavidsaver@gmail.com>
2025-05-14 17:17:24 -0700

    app

--- README.md
+++ README.md
@@ -2,3 +2,57 @@
 
 An example of an EPICS IOC using the modbus driver to communicate
 with a simulation modbus server.
+
+# Requirements
+
+Tested with Debian 12 on amd64 host.
+
+```sh
+sudo apt-get install git build-essential libreadline-dev python3-pymodbus
+```
+
+# Setup
+
+```sh
+git clone --recursive https://github.com/mdavidsaver/epics-modbus-example
+cd epics-modbus-example
+./build.sh -j2
+```
+
+# Run
+
+In one terminal, run the simulation server.
+
+```sh
+cd epics-modbus-example
+./modbus-server.py
+```
+
+In another terminal, run the IOC.
+
+```sh
+cd epics-modbus-example/ioc/iocBoot/iocexample
+./st.cmd
+```
+
+In a third terminal run a client
+
+```sh
+cd epics-modbus-example
+./epics-base/bin/*/camonitor TST:COUNT:I
+```
+
+# Changing
+
+Interesting files:
+
+- `epics-modbus-example/ioc/iocBoot/iocexample/st.cmd`
+- `epics-modbus-example/ioc/mbusApp/Db/modbus-server.substitutions`
+- `epics-modbus-example/ioc/mbusApp/Db/int16.db`
+
+Changes under `mbusApp/` require a build.
+
+```sh
+cd epics-modbus-example
+./build.sh -j2
+```
--- ioc/iocBoot/iocexample/st.cmd
+++ ioc/iocBoot/iocexample/st.cmd
@@ -1,22 +1,24 @@
 #!../../bin/linux-x86_64/mbus
 
-#- SPDX-FileCopyrightText: 2005 Argonne National Laboratory
-#-
-#- SPDX-License-Identifier: EPICS
+epicsEnvSet("DEVICE_ADDR", "127.0.0.1:5020")
+epicsEnvSet("DEVICE_POLL", "100") # polling interval (ms)
+epicsEnvSet("DEVICE_TIMEOUT", "70") # ms
 
-#- You may have to change mbus to something else
-#- everywhere it appears in this file
-
-#< envPaths
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "../../db")
 
 ## Register all support components
 dbLoadDatabase "../../dbd/mbus.dbd"
 mbus_registerRecordDeviceDriver(pdbbase) 
 
-## Load record instances
-#dbLoadRecords("../../db/mbus.db","user=dev")
+# Configure communication with a server
+drvAsynIPPortConfigure("DEV", "$(DEVICE_ADDR)")
+asynSetOption("DEV",0, "disconnectOnReadTimeout", "Y")
+modbusInterposeConfig("DEV", 0, $(DEVICE_TIMEOUT))
+
+# Configure register range(s)
+# read holding register -> code 3
+drvModbusAsynConfigure("DEV_HR", "DEV", 0, 3, 0, 64, 0, $(DEVICE_POLL))
+
+dbLoadRecords("modbus-server.db", "PORT=DEV_HR,P=TST:")
 
 iocInit()
-
-## Start any sequence programs
-#seq sncmbus,"user=dev"
